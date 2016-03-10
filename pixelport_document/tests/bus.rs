5f290c28616154fedf44a70cdc107b0feac82880
Fredrik Noren <fredrik.jw.noren@gmail.com>
2016-03-09 21:22:30 -0500

    document: Remove old test

--- pixelport_document/tests/bus.rs
+++ pixelport_document/tests/bus.rs
@@ -3,26 +3,6 @@ extern crate pixelport_document;
 
 use pixelport_document::*;
 
-
-#[test]
-fn test() {
-    // let mut bus: Bus<String> = Bus::new();
-    //
-    // #[derive(PartialEq, Debug, Clone)]
-    // struct Uniforms {
-    //     bones: Vec<i32>
-    // }
-    // bus.set(&"uniforms".to_string(), vec!["bones".to_string()], true, Box::new(|bus| {
-    //     Box::new(Uniforms { bones: bus.get_typed::<Vec<i32>>(&"bones".to_string()).expect("No bones?") })
-    // }));
-    //
-    // bus.set(&"bones".to_string(), Vec::new(), false, Box::new(|bus| Box::new(vec![5, 3, 10, 3])));
-    //
-    // let uniforms = bus.get_typed::<Uniforms>(&"uniforms".to_string()).expect("No uniform?");
-    // assert_eq!(uniforms, Uniforms { bones: vec![5, 3, 10, 3] });
-    // //assert_eq!(bus.invalidations_log, vec![ChangedNonZero { added: Vec::new(), removed: Vec::new() }]);
-}
-
 #[test]
 fn test_entry_of_wrong_type() {
     let mut bus: Bus = Bus::new();
